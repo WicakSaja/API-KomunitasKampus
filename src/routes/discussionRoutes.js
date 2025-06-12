b37d986e8014ba89e416a1d54aa8e049b2b2d204
WicakSaja <bayubayws@gmail.com>
2025-06-13 00:03:51 +0700

    menambahkan fitur komentar pada diskusi

--- src/routes/discussionRoutes.js
+++ src/routes/discussionRoutes.js
@@ -13,6 +13,10 @@ router.post('/', requireAuth, validate(discussionSchema), createDiscussion);
 router.put('/:id', requireAuth, requireOwner('discussion'), validate(discussionSchema), updateDiscussion);
 router.delete('/:id', requireAuth, requireOwner('discussion'), deleteDiscussion);
 
+// Menambah komentar pada diskusi dengan parameter id diskusi
+router.post('/:id/comment', requireAuth, validate(commentSchema), addComment);
+// menghapus komentar
+router.delete('/comment/:id', requireAuth, requireOwner('comment') , deleteComment);
 
 
 export default router;
--- src/validations/discussionValidation.js
+++ src/validations/discussionValidation.js
@@ -4,3 +4,7 @@ export const discussionSchema = Joi.object({
   title: Joi.string().required(),
   content: Joi.string().required(),
 });
+
+export const commentSchema = Joi.object({
+  content: Joi.string().required(),
+});
